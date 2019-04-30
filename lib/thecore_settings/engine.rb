8f9a7ea74c42f266a043d83445a343b46473c2ed
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-04-30 16:37:26 +0200

    migration initializer for engine cleanup

--- lib/thecore_settings/engine.rb
+++ lib/thecore_settings/engine.rb
@@ -1,7 +1,7 @@
 module ThecoreSettings
   class Engine < ::Rails::Engine
     initializer "thecore_settings.add_to_migrations" do |app|
-      unless app.root.to_s == root.to_s
+      unless app.root.to_s.match root.to_s
         # APPEND TO MAIN APP MIGRATIONS FROM THIS GEM
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
