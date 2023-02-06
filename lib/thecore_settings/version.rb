f3d55123d4f6ddfd7fc30a00364770caa8a5d1ba
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-02-06 17:34:29 +0000

    Put gem version on git tag

--- lib/thecore_settings/version.rb
+++ lib/thecore_settings/version.rb
@@ -1,4 +1,3 @@
 module ThecoreSettings
-  # VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`.chomp}"
-  VERSION = "3.0.0"
+  VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`.chomp}"
 end
